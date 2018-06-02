b0b8e4b1cc24be16a341b64cc3200a1f7f6d730c
laloceh <laloceh@hotmail.com>
2018-06-02 10:59:20 -0600

    add open and save as file functions

---
+++ open_file_window.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  2 10:31:47 2018
+
+@author: eduardo
+"""
+
+def open_files():
+    """Returns list of filenames+paths given starting dir"""
+    import Tkinter
+    import tkFileDialog
+
+    root = Tkinter.Tk()
+    root.withdraw()
+    root.overrideredirect(True)
+    root.geometry('0x0+0+0')
+    
+    root.deiconify()
+    root.lift()
+    root.focus_force()
+    
+    filenames = tkFileDialog.askopenfilenames(parent=root, title = "Open file")
+    root.destroy()
+    
+    return filenames[0]
+
+def save_to_files():
+    """Returns list of filenames+paths given starting dir"""
+    import Tkinter
+    import tkFileDialog
+
+    root = Tkinter.Tk()
+    root.withdraw()
+    root.overrideredirect(True)
+    root.geometry('0x0+0+0')
+    
+    root.deiconify()
+    root.lift()
+    root.focus_force()
+    
+    #filenames = tkFileDialog.asksaveasfilename(initialdir = "/",title = "Select file",filetypes = (("text files","*.txt"),("all files","*.*")))
+    filenames = tkFileDialog.asksaveasfilename(parent=root, title = "Save file as")
+    
+    #filenames = tkFileDialog.askopenfilenames(parent=root)
+    root.destroy()
+    
+    return filenames
+ 
+
+inputfile = open_files()
+print inputfile
+
+outputfile = save_to_files()
+print outputfile
